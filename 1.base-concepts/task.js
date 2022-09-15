c8b7dd080d0cd1f5f224f7b11e2ca0ed4fc8692d
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-09-15 20:43:17 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,38 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let d = b ** 2 - 4 * a * c;
+  let arr = [];
+  if (d === 0) {
+    let rootOne = -b / (2 * a);
+    arr.push(rootOne);
+  } else if (d > 0) {
+    let rootOne = (-b + Math.sqrt(d)) / (2 * a);
+    let rootTwo = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(rootOne, rootTwo);
+  }
   return arr; // array
 }
 
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  let checkPercent = Number(percent) / 100 / 12;
+  let checkContribution = Number(contribution);
+  let checkAmount = Number(amount);
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  if (isNaN(checkPercent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(checkContribution)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(checkAmount)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  let bodyCredit = checkAmount - checkContribution;
+  let carrentDate = new Date();
+  let creditTern = date.getMonth() - carrentDate.getMonth() + (12 * (date.getFullYear() - carrentDate.getFullYear()));
+  let monthlyFee = bodyCredit * (checkPercent + (checkPercent / (((1 + checkPercent) ** creditTern) - 1)));
+  totalAmount = (creditTern * monthlyFee).toFixed(2);
+  return Number(totalAmount);
 }
