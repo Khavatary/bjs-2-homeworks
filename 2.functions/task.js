1ddb13a27ba92453918c9fa11e83eddcc56b17ff
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-09-20 22:38:26 +0300

    editHomeWork2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,17 +4,7 @@ function getArrayParams(arr) {
   let max = arr[0];
   let sum = 0;
 
-  (function () {
-    if (arr.length === 0) {
-      return {
-        min: 0,
-        max: 0,
-        avg: 0
-      };
-    }
-  }());
-
-  for (i = 0; i < arr.length; i++) {
+  for ( let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i]
     } else if (arr[i] < min) {
@@ -35,8 +25,8 @@ function getArrayParams(arr) {
 // Задание 2
 function worker(arr) {
   let sum = 0;
-  for (t = 0; t < arr.length; t++) {
-    sum += arr[t]
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
   }
   return sum;
 }
@@ -44,7 +34,7 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max = -Infinity;
   let temp;
-  for (i = 0; i < arrOfArr.length; i++) {
+  for (let i = 0; i < arrOfArr.length; i++) {
     temp = func(arrOfArr[i])
     if (temp > max) {
       max = temp;
@@ -55,16 +45,14 @@ function makeWork(arrOfArr, func) {
 
 // Задание 3
 function worker2(arr) {
-  let diff;
   let min = arr[0];
   let max = arr[0];
-  for (x = 0; x < arr.length; x++) {
-    if (arr[x] > max) {
-      max =arr[x]
-    } else if (arr[x] < min) {
-      min = arr[x]
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max =arr[i]
+    } else if (arr[i] < min) {
+      min = arr[i]
     }
   }
-  diff = Math.abs(max - min);
-  return diff;
+  return Math.abs(max - min);
 }
