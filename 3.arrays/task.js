85237d7f77ab954db24c291e9ce5b245972e8b13
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-09-22 20:45:32 +0300

    homeWork3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+function compareArrays(arr1, arr2) { 
+  return arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i]);
 }
 
-function advancedFilter(arr) {
+/* function advancedFilter(arr) {
   let resultArr;
+  resultArr = arr.filter(number => number > 0);
+  resultArr = resultArr.filter(number => number % 3 == 0);
+  return resultArr.map( number => number * 10);
+} */
 
-  // Ваш код
-
-  return resultArr; // array
+function advancedFilter(arr) {
+  return (arr.filter(number => number > 0 && number % 3 == 0)).map( number => number * 10);
 }
