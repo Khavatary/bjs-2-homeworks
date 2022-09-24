7ae8f9a830f2957fd10f9052e6a967a74a74c0e1
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-09-24 21:10:12 +0300

    homeWork4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,34 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...args) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  for (let arg of args) this.marks.push(arg);
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+  delete Student.mark;
+  this.excluded = reason;
+}
