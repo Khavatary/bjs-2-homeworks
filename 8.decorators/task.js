4aa76740147c0dd2618b7954e74f89f268c3fdbc
Alekssandr Shitov <shitovaleksand@mail.ru>
2022-10-11 22:27:50 +0300

    homeWork8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,46 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find(item => item.hash === hash);;
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+    let result = func(...args);
+    cache.push({
+      hash,
+      result
+    });
+    if (cache.length > 5) {
+      cache.shift()
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  function wrapper(...args) {
+    wrapper.allCount++;
+    if (timeoutId === null) {
+      func.call(this, ...args);
+    }
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      timeoutId = null;
+      func.call(this, ...args);
+    }, delay);
+    console.log(wrapper.count);
+    console.log(wrapper.allCount);
+  }
+  return wrapper;
 }
